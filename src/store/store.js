358c6da86650231eed2c92bbc7735aad8105065f
David Sauntson <david@landd-agency.co.uk>
2017-07-16 22:12:51 +0100

    added strore

---
+++ src/store/store.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+
+import rootReducer from '../reducers/rootReducer';
+import thunkMiddleware from 'redux-thunk';
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+	rootReducer,
+	composeEnhancers(applyMiddleware(
+		thunkMiddleware
+	))
+);
+
+export default store;
