a6e7c0c9953dbe07d2d62130eb9ef115facc517c
David Sauntson <david@landd-agency.co.uk>
2017-07-16 21:55:34 +0100

    added subjects

---
+++ src/data/subjects.js
@@ -0,0 +1,16 @@
+export const ENDINGS = [
+    { text: 'you', shouldUseAltEnding: true }, 
+    { text: 'your mother', shouldUseAltEnding: false }, 
+    { text: 'the govenment', shouldUseAltEnding: false }, 
+    { text: 'celebrities', shouldUseAltEnding: false }, 
+    { text: 'Ariana Grande', shouldUseAltEnding: true }, 
+    { text: 'scientists', shouldUseAltEnding: true }, 
+    { text: 'childhood filmstars', shouldUseAltEnding: false }, 
+    { text: 'the world', shouldUseAltEnding: false }, 
+    { text: 'remotes tribesmen', shouldUseAltEnding: true }, 
+    { text: 'LGBTQ people', shouldUseAltEnding: true }, 
+    { text: 'Donald Trump', shouldUseAltEnding: false }, 
+    { text: 'facists', shouldUseAltEnding: true }, 
+    { text: 'liberals', shouldUseAltEnding: true }, 
+    { text: 'North Londoners', shouldUseAltEnding: true }, 
+]
